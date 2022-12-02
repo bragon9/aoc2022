31789a32a35702feda808dfe1df4f48f7aaaf1b4
Brandon Spagon <brandon.spagon@openly.com>
2022-12-02 08:34:42 -0600

    rework main

--- main.go
+++ main.go
@@ -6,10 +6,31 @@ import (
 	"log"
 )
 
-func main() {
-	ans, err := day1.Part2()
+type Answer struct {
+	Part1 any
+	Part2 any
+}
+
+func getDay1() (Answer, error) {
+	answer := Answer{}
+	ans1, err := day1.Part1()
 	if err != nil {
-		log.Fatalf(err.Error())
+		return Answer{}, err
 	}
-	fmt.Println(ans)
+	answer.Part1 = ans1
+	ans2, err := day1.Part2()
+	if err != nil {
+		return Answer{}, err
+	}
+	answer.Part2 = ans2
+
+	return answer, nil
+}
+
+func main() {
+	Day1, err := getDay1()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Day1: \n%+v", Day1)
 }
