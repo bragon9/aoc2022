8a33bb5c7c07b8311ab0408f8b7b4c1cba70bf47
Brandon Spagon <brandon.spagon@openly.com>
2022-12-09 11:54:09 -0600

    solve d6p2

--- pkg/days/day6/day6.go
+++ pkg/days/day6/day6.go
@@ -4,6 +4,11 @@ import (
 	"aoc2022/pkg/inputreader"
 )
 
+const (
+	packetLength  = 4
+	messageLength = 14
+)
+
 type window struct {
 	set  map[rune]int
 	dups int
@@ -27,14 +32,14 @@ func (w *window) HasDup() bool {
 	return w.dups != 0
 }
 
-func findMarker(line string) int {
+func findMarker(line string, length int) int {
 	var markerIndex int
 
 	w := &window{set: make(map[rune]int, 26)}
 	for i, r := range line {
 		w.Add(r)
-		if i > 3 {
-			w.Remove(rune(line[i-4]))
+		if i > length-1 {
+			w.Remove(rune(line[i-length]))
 			if !w.HasDup() {
 				markerIndex = i + 1
 				break
@@ -51,11 +56,16 @@ func Part1() (int, error) {
 		return 0, err
 	}
 
-	markerIndex := findMarker(lines[0])
+	markerIndex := findMarker(lines[0], packetLength)
 	return markerIndex, nil
 }
 
 func Part2() (int, error) {
+	lines, err := inputreader.ReadLines("inputs/day6/1.txt")
+	if err != nil {
+		return 0, err
+	}
 
-	return 0, nil
+	markerIndex := findMarker(lines[0], messageLength)
+	return markerIndex, nil
 }
